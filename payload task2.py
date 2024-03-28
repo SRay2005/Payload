bedf4cf6e2955435977369dcf30ca1da2239c179
SRay2005 <145295410+SRay2005@users.noreply.github.com>
2024-03-28 19:27:18 +0530

    payload task2.py

--- payload task2.py
+++ payload task2.py
@@ -3,12 +3,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 
-# Step 1: Load the Iris dataset
+
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-# Step 2: No preprocessing needed for this dataset
+
 
 # Step 3: Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
